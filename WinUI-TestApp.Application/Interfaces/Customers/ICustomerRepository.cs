615f260a71854821c7782911f460914a555ad3d3
Love Hedström <lovehedstrom@live.com>
2026-09-23 14:02:47 +0200

    Add interface for repository

--- WinUI-TestApp.Application/Interfaces/Customers/ICustomerRepository.cs
+++ WinUI-TestApp.Application/Interfaces/Customers/ICustomerRepository.cs
@@ -1,5 +1,32 @@
 ﻿
+using System;
+using System.Collections.Generic;
+using WinUI_TestApp.Domain.Models.Customers;
+
 namespace WinUI_TestApp.Application.Interfaces.Customers;
 public interface ICustomerRepository
 {
+//----------- Create ----------- 
+
+    //Lägg till i listan
+    public void CreateCustomer(Customer customer);
+
+//----------- Read ----------- 
+
+    //Hämta kund med id
+    public Customer GetCustomerById(Guid customerId);
+
+    public List<Customer> GetAllCustomers();
+
+
+    //----------- Update ----------- 
+
+    //Uppdatera - hitta kund, ändra värden och spara igen
+    public void UpdateCustomer(Customer customer);
+
+//----------- Delete -----------
+
+    //Ta bort customer med hjälp av ID
+    public void DeleteCustomer(Guid customerId);
+
 }
--- WinUI-TestApp.Infrastructure/Repositories/Customers/CustomerRepository.cs
+++ WinUI-TestApp.Infrastructure/Repositories/Customers/CustomerRepository.cs
@@ -2,4 +2,8 @@
 namespace WinUI_TestApp.Infrastructure.Repositories.Customers;
 public class CustomerRepository
 {
+    //Create
+    //Read
+    //Update
+    //Delete
 }
